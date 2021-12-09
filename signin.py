cc147c58ec2328acbe419f11442cac6fda12f0d1
Florence <62096528+flrnc@users.noreply.github.com>
2021-12-09 08:51:16 +0800

    Update signin.py

--- signin.py
+++ signin.py
@@ -25,21 +25,25 @@ class Signin:
 
         # Username
         lbl_user = Label(Frame_login, text="Username", font=("Montserrat", 12, "bold"), fg="grey",
-                         bg="white").place(x=70, y=140)
+                         bg="white").place(x=70, y=130)
         self.username = Entry(Frame_login, font=("Montserrat", 12), bg="white")
-        self.username.place(x=70, y=170, width=370, height=35)
+        self.username.place(x=70, y=160, width=370, height=35)
 
         # Password
         lbl_pass = Label(Frame_login, text="Password", font=("Montserrat", 12, "bold"), fg="grey",
-                         bg="white").place(x=70, y=210)
+                         bg="white").place(x=70, y=205)
         self.password = Entry(Frame_login, font=("Montserrat", 12), bg="white")
-        self.password.place(x=70, y=240, width=370, height=35)
+        self.password.place(x=70, y=235, width=370, height=35)
+
 
         # Button
         login = Button(Frame_login, command=self.verify, cursor="hand2", text="Login", bd=0,
                        font=("Montserrat", 15), bg="black", fg="white").place(x=70, y=320, width=180, height=40)
         signup = Button(Frame_login, cursor="hand2", text="Signup", bd=0, font=("Montserrat", 15), bg="black",
                         fg="white").place(x=260, y=320, width=180, height=40)
+        forgot = Button(Frame_login, cursor="hand2", text="Forgot Password?", bd=0, font=("Montserrat", 10), bg="white",
+                        fg="grey", activebackground="white").place(x=320, y=280, width=120, height=20)
+
 
     def verify(self):
         if self.username.get() == "" or self.password.get() == "":
@@ -49,7 +53,7 @@ class Signin:
             self.feature_window()
         else:
             messagebox.showerror("Error", "Invalid Username or Password") #, parent=self.root)
-            
+
     def feature_window(self):
         self.new_window = Toplevel(self.root)
         self.app = Features(self.new_window)
