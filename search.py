58102ba59e041e6feac8bc6187222744fcb396d8
Florence <62096528+flrnc@users.noreply.github.com>
2021-12-12 16:08:14 +0800

    Update search.py

--- search.py
+++ search.py
@@ -12,6 +12,13 @@ class Search:
         self.bg = ImageTk.PhotoImage(file="bioID2.png")
         self.bg_image = Label(self.root, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
 
+        # back
+        back = Image.open(r"C:\Users\Florence\PycharmProjects\pythonProject\venv\back.png")
+        back = back.resize((70, 70), Image.ANTIALIAS)
+        self.photoback = ImageTk.PhotoImage(back)
+        back_btn = Button(root, image=self.photoback, bd=0, cursor="hand2")
+        back_btn.place(x=20, y=34, width=70, height=70)
+
 if __name__ == "__main__":
     root = Tk()
     obj = Search(root)
