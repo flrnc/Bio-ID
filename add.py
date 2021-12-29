c832f1365587dfff27bfd707323f48d549391339
Florence <62096528+flrnc@users.noreply.github.com>
2021-12-29 22:32:36 +0800

    Update add.py

--- add.py
+++ add.py
@@ -9,6 +9,7 @@ import cv2
 import os
 import numpy as np
 
+
 class Add:
     def __init__(self, root):
         self.root = root
@@ -85,7 +86,7 @@ class Add:
         entry_addrs.grid(row=7, column=1, sticky=W)
 
         # Buttons
-        btn_capture = Button(root, text="Take picture", command=self.generate_data, font=("Montserrat", 12, "bold"), bg="black", fg="white")
+        btn_capture = Button(root, command=self.generate_data, text="Take picture", font=("Montserrat", 12, "bold"), bg="black", fg="white")
         btn_capture.place(x=11, y=575, width=338, height=80)
 
         btn_train = Button(root, text="Train", command=self.train_classifier, font=("Montserrat", 12, "bold"), bg="black", fg="white")
@@ -103,8 +104,9 @@ class Add:
         btn_reset = Button(root, text="Reset", command=self.reset_data, font=("Montserrat", 12, "bold"), bg="black", fg="white")
         btn_reset.place(x=870, y=685, width=350, height=80)
 
+
         # back
-        back = Image.open(r"back.png")
+        back = Image.open(r"C:\Users\Florence\PycharmProjects\pythonProject\venv\back.png")
         back = back.resize((70, 70), Image.ANTIALIAS)
         self.photoback = ImageTk.PhotoImage(back)
         back_btn = Button(root, image=self.photoback, bd=0, cursor="hand2")
@@ -157,7 +159,7 @@ class Add:
             messagebox.showerror("Error", "All Fields are required.", parent=self.root)
         else:
             try:
-                conn=mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143", database="bioid")
+                conn=mysql.connector.connect(host="localhost", username="root", password="Mamadaw12!", database="bioid")
                 my_cursor=conn.cursor()
                 my_cursor.execute("insert into resident values(%s,%s,%s,%s,%s,%s,%s,%s)", (
                                                                                             self.var_resnum.get(),
@@ -172,14 +174,14 @@ class Add:
                 conn.commit()
                 self.fetch_data()
                 conn.close()
-                messagebox.showinfo("Success", "Resident details has been added successfully", parent=self.root)
+                messagebox.showinfo("Success", "Resident has been added successfully", parent=self.root)
                 self.reset_data()
             except Exception as es:
                 messagebox.showerror("Error", f"Due to :{str(es)}", parent=self.root)
 
     # fetch data = yung saved data sa db, mapupunta doon sa frame (yung naviview yung input data)
     def fetch_data(self):
-        conn = mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143", database="bioid")
+        conn = mysql.connector.connect(host="localhost", username="root", password="Mamadaw12!", database="bioid")
         my_cursor = conn.cursor()
         my_cursor.execute("select * from resident")
         data=my_cursor.fetchall()
@@ -212,9 +214,9 @@ class Add:
             messagebox.showerror("Error", "Select a specific resident.", parent=self.root)
         else:
             try:
-                update=messagebox.askyesno("Update", "Do you want to update this resident details?", parent=self.root)
+                update=messagebox.askyesno("Update", "Do you want to update this resident?", parent=self.root)
                 if update>0:
-                    conn = mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143", database="bioid")
+                    conn = mysql.connector.connect(host="localhost", username="root", password="Mamadaw12!", database="bioid")
                     my_cursor = conn.cursor()
                     my_cursor.execute("update resident set Name=%s,DOB=%s,Sex=%s,Nationality=%s,CPNum=%s,Email=%s,Address=%s where ResNum=%s",(
 
@@ -246,7 +248,7 @@ class Add:
             try:
                 delete = messagebox.askyesno("Delete Resident", "Do you want to delete this resident?",parent=self.root)
                 if delete > 0:
-                    conn = mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143",database="bioid")
+                    conn = mysql.connector.connect(host="localhost", username="root", password="Mamadaw12!",database="bioid")
                     my_cursor = conn.cursor()
                     sql="delete from resident where ResNum=%s"
                     val=(self.var_resnum.get(),)
@@ -280,7 +282,7 @@ class Add:
             messagebox.showerror("Error", "Select a specific resident.", parent=self.root)
         else:
             try:
-                conn = mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143",database="bioid")
+                conn = mysql.connector.connect(host="localhost", username="root", password="Mamadaw12!",database="bioid")
                 my_cursor = conn.cursor()
                 my_cursor.execute("select * from resident")
                 myresult = my_cursor.fetchall()
@@ -288,130 +290,94 @@ class Add:
                 for x in myresult:
                     id+=1
                 my_cursor.execute("update resident set Name=%s,DOB=%s,Sex=%s,Nationality=%s,CPNum=%s,Email=%s,Address=%s where ResNum=%s",
-                    (
-
-                        self.var_name.get(),
-                        self.var_dob.get(),
-                        self.var_sex.get(),
-                        self.var_nationality.get(),
-                        self.var_num.get(),
-                        self.var_email.get(),
-                        self.var_address.get(),
-                        self.var_resnum.get()
-                    ))
+                                                                                            (
+                                                                                            self.var_name.get(),
+                                                                                            self.var_dob.get(),
+                                                                                            self.var_sex.get(),
+                                                                                            self.var_nationality.get(),
+                                                                                            self.var_num.get(),
+                                                                                            self.var_email.get(),
+                                                                                            self.var_address.get(),
+                                                                                            self.var_resnum.get()==id+1
+                                                                                            ))
                 conn.commit()
                 self.fetch_data()
                 self.reset_data()
                 conn.close()
 
+                # load predefined face classifier or detector
                 face_classifier = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
                 def face_cropped(img):
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #convert to gray scale
+                    faces = face_classifier.detectMultiScale(gray,1.3,5) #detect the face
+                    # scaling factor - how much the image size is reduced at each image scale=1.3
+                    # minimum neighbor - how many neighbors each candidate rectangle should have to retain it=5
 
-                    for (x, y, w, h) in faces:
-                        face_cropped = img[y:y + h, x:x + w]
+                    # iterate over faces
+                    for (x,y,w,h) in faces:
+                        face_cropped = img[y:y+h, x:x+w] #to draw the rectangle
                         return face_cropped
 
-                cap = cv2.VideoCapture(0)
-                img_id = 0
-                while True:
-                    ret, my_frame = cap.read()
+                cap = cv2.VideoCapture(0) # define a video capture object
+                img_id = 0 # for samples
+                while True: # while true, if camera is working/opened
+                    ret, my_frame = cap.read() # read every frame which will be stored in my_frame
                     if face_cropped(my_frame) is not None:
-                        img_id += 1
-                        face = cv2.resize(face_cropped(my_frame), (450, 450),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+                        img_id += 1 # incrementing sample number
+                        # resize frame, INTER_CUBIC - interpolation over 4×4 pixel neighborhood, for enlarging the image
+                        face = cv2.resize(face_cropped(my_frame), (450,450),fx=0,fy=0, interpolation = cv2.INTER_CUBIC)
+                        # convert to gray scale
                         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+                        # saving the captured face image in the dataset folder
                         file_name_path = "data/user." + str(id) + "." + str(img_id) + ".jpg"
                         cv2.imwrite(file_name_path, face)
+                        # this is just a font design
                         cv2.putText(face, str(img_id), (50,50), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 2)
-                        cv2.imshow("Scanning face.....", face)
+                        cv2.imshow("Scanning face.....", face) # to show the camera frame
 
+                    # waitKey to display the window for 100 milliseconds
+                    # 's' button to quit
+                    # take 50 samples
                     if cv2.waitKey(1) == ord('s') or int(img_id) == 50:
                         break
 
-                cap.release()
-                cv2.destroyAllWindows()
-                messagebox.showinfo("Result", "Generating photo sets completed")
+                cap.release() # after the loop release the capture object
+                cv2.destroyAllWindows() #destroy the window
+                messagebox.showinfo("Result", "Generating photo sets completed!")
             except Exception as es:
                 messagebox.showerror("Error", f"Due to: {str(es)}", parent=self.root)
 
-
+    # training the image dataset
     def train_classifier(self):
-        data_dir=("data")
+        data_dir=("data") # image dataset folder
+        # path.join() - combines path names into one complete path
+        # listdir() - to get the list of all files and directories in the specified directory
         path=[os.path.join(data_dir,file) for file in os.listdir(data_dir)]
+        # initialize lists to store extracted faces and associated ids
         faces=[]
         ids=[]
 
         for image in path:
-            img=Image.open(image).convert('L')
+            img=Image.open(image).convert('L') #convert image to gray scale
             imageNp=np.array(img, 'uint8')
             id=int(os.path.split(image)[1].split('.')[1])
 
             faces.append(imageNp)
             ids.append(id)
-            cv2.imshow("Training", imageNp)
+            cv2.imshow("Training",imageNp)
             cv2.waitKey(1) == 13
         ids=np.array(ids)
 
-        #=================================
-        clf = cv2.face.LBPHFaceRecognizer_create()
-        clf.train(faces, ids)
-        clf.write("classifier.xml")
-        cv2.destroyAllWindows()
+        # Train the classifier and save
+        clf=cv2.face.LBPHFaceRecognizer_create()
+        clf.train(faces,ids) # training the classifier using .train
+        clf.write("classifier.xml") # after training, store the classifier in xml file
+        cv2.destroyAllWindows() # destory the window
         messagebox.showinfo("Result","Training completed!")
 
 
-    '''def face_recog(self):
-        def draw_boundary(img, classifier,scaleFactor, minNeighbors, color, text, clf):
-            gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            features = classifier.detectMultiScale(gray_image, scaleFactor, minNeighbors)
 
-            coord=[]
-            
-            for (x, y, w, h) in features:
-                cv2.rectangle(img(x, y), (x+w, y+h), (0,255,0), 3)
-                id,predict=clf.predict(gray_image[y:y+h, x:x+w])
-                confidence=int((100*(1-predict/300)))
-
-                conn=mysql.connector.connect(host="localhost", username="root", password="1l0v3h0td0g_143",database="bioid")
-                my_cursor=conn.cursor()
-
-                my_cursor.execute("select from resident where ResNum=%s")
-                i=my_cursor.fetchone()
-                i="+".join(i)
-
-
-
-                if confidence>77:
-                    cv2.putText(img, f"resident:{i}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255),3)
-                else:
-                    cv2.rectangle(img(x,y),(x+w,y+h),(0,0,255),3)
-                    cv2.putText(img, f"resident:{i}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX, 0.8, (255,255,255),3)
-
-                coord=[x, y, w, h]
-
-            return coord
-
-        def recognize(img, clf, faceCascade):
-            coord=draw_boundary(img, faceCascade,1.1,10,(255,25,255),"Face",clf)
-            return img
-
-        faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-        clf=cv2.face.LBPHFaceRecognizer_create()
-        clf.read("classifier.xml")
-
-        video_cap=cv2.VideoCapture(0)
-
-        while True:
-            ret, img=video_cap.read()
-            img=recognize(img,clf,faceCascade)
-            cv2.imshow("Welcome daw", img)
-
-            if cv2.waitKey(1)==13:
-                break
-        video_cap.release()
-        cv2.destroyAllWindows()'''
 if __name__ == "__main__":
     root = Tk()
     obj = Add(root)
